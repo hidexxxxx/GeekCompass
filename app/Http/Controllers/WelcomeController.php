67fbcf392acebbbbb55e07ce209a5b5b49c89eb1
hidexxxxx <hidetaka54522@gmail.com>
2024-02-24 10:30:13 +0900

    試し

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,9 +6,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
+
 {
-    public function index()
+    public function __invoke(Request $request)
     {
         return view('welcome');
     }
 }
+
+// {
+//     public function index()
+//     {
+//         return view('welcome');
+//     }
+// }
