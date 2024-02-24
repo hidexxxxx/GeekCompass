ae20d187851e23e1dc948840a002bb5b466df40f
hidexxxxx <hidetaka54522@gmail.com>
2024-02-24 11:02:11 +0900

    ルート設定戻し

--- routes/web.php
+++ routes/web.php
@@ -64,12 +64,12 @@ use App\Http\Controllers\AboutUsController;
 // Route::get('/PrivacyPolicy', 'App\Http\Controllers\PrivacyPolicyController@index');
 // Route::get('/AboutUs', 'App\Http\Controllers\AboutUsController@index');
 
-// Route::get('/', [WelcomeController::class, 'index']);
-// Route::get('/welcome', [WelcomeController::class, 'index']);
-// Route::get('/HowtoUse', [HowtoUseController::class, 'index']);
-// Route::get('/terms&conditions', [TermsAndConditionsController::class, 'index']);
-// Route::get('/PrivacyPolicy', [PrivacyPolicyController::class, 'index']);
-// Route::get('/AboutUs', [AboutUsController::class, 'index']);
+Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/welcome', [WelcomeController::class, 'index']);
+Route::get('/HowtoUse', [HowtoUseController::class, 'index']);
+Route::get('/terms&conditions', [TermsAndConditionsController::class, 'index']);
+Route::get('/PrivacyPolicy', [PrivacyPolicyController::class, 'index']);
+Route::get('/AboutUs', [AboutUsController::class, 'index']);
 
 // Route::resource('/', 'WelcomeController');
 // Route::resource('/welcome', 'WelcomeController');
